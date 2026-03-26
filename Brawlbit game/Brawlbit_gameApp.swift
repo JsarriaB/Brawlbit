b7ff226676f427ac9096552b76788c57447c9307
JorgeSB <jsarriab28@gmail.com>
2026-03-26 17:33:58 +0100

    Initial Commit

---
+++ Brawlbit game/Brawlbit_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brawlbit_gameApp.swift
+//  Brawlbit game
+//
+//  Created by Jorge Sarria Bruned on 26/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Brawlbit_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
